d4a5bf15126e04d8ea3e500a77eaf9db83f8a724
vidwalk <claudiurediu@gmail.com>
2020-08-18 13:58:19 +0200

    Added goat counter and copyright

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Claudiu Rediu'
 SITENAME = 'Vidwalk'
+FOOTER_TEXT = '© CLAUDIU REDIU 2020'
 SITEURL = 'https://vidwalk.github.io/vidwalkBlog'
 #for demo comment top. for release comment bottom
 #SITEURL = ''
