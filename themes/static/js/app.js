c02a18ac47576d70e9674c811eb9421dbb6bc19e
vidwalk <claudiurediu@gmail.com>
2020-08-18 14:41:18 +0200

    Added contacting implementation through formspree

--- themes/static/js/app.js
+++ themes/static/js/app.js
@@ -53,3 +53,53 @@ var tl = gsap.timeline({defaults: {duration: 1 }});
 
 tl.from(".post",{x:50, stagger:.6,opacity:0})
 .from(".sidebar-sticky",{y:-50,stagger:.6,opacity:0},'-=2');
+
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("contact-form");
+    var button = document.getElementById("contact-button");
+    var statusSuccess = $('.status-message .success-para');
+    var statusError = $('.status-message .error-para');
+    statusSuccess.empty();
+    statusError.empty();
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      statusSuccess.append("Thanks, I will get back at you in max 24hr!");
+    }
+
+    function error() {
+        statusError.append("Oops! There was a problem.");
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
